29dec8bcb9aa9de6b13306db6a33502956909c92
Cypher <futuhdware@zoho.com>
2020-07-23 00:49:27 +0100

    1.0 upload
    1.0 upload

---
+++ Missions/DayZCommunityOfflineMode.valning/core/BaseModuleInclude.c
@@ -0,0 +1,5 @@
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.valning\\core\\ModuleManager.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.valning\\core\\StaticFunctions.c"
+
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.valning\\core\\CommunityOfflineClient.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.valning\\core\\CommunityOfflineServer.c"
